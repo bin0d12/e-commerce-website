d55bd6374d93df8c3947f9cbde246d957002c602
githubb <binodkumarsahu951@gmail.com>
2023-03-27 11:20:34 +0530

    store local to db.

--- e-commerceProject/src/app/user-auth/user-auth.component.ts
+++ e-commerceProject/src/app/user-auth/user-auth.component.ts
@@ -1,42 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { cart } from '../interface/cart-interface';
 import { LogIn } from '../interface/logIn-interface';
+import { product } from '../interface/product-interface';
 import { SignUp } from '../interface/signup-interface';
+import { ProductsService } from '../services/products.service';
 import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
-  styleUrls: ['./user-auth.component.css']
+  styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent implements OnInit {
   showLogin: boolean = true;
-  authError: string = ""
-  constructor(private userService: UserService) { }
+  authError: string = '';
+  constructor(private userService: UserService, private productService: ProductsService) {}
 
-  ngOnInit(): void { 
-    this.userService.userAuthReload()
+  ngOnInit(): void {
+    this.userService.userAuthReload();
   }
-  signUp(data: SignUp){
-    this.userService.userSignUp(data)
+  signUp(data: SignUp) {
+    this.userService.userSignUp(data);
   }
-  userLogIn(data: LogIn){
-    this.userService.userLogIn(data)
+  userLogIn(data: LogIn) {
+    this.userService.userLogIn(data);
     this.userService.invalidUserAuth.subscribe((result) => {
-      if(result){
-        this.authError = "please enter valid user details"
+      if (result) {
+        this.authError = 'please enter valid user details';
+      } else {
+        this.localCartToRemoteCart()
       }
-      
-    })
+    });
     // this.userService.userLogIn(data).subscribe((payLoad) =>{
-      
+
     // })
   }
-  openLogin(){
-    this.showLogin = true
-
+  openLogin() {
+    this.showLogin = true;
   }
-  openSignUp(){
-    this.showLogin = false
-
+  openSignUp() {
+    this.showLogin = false;
+  }
+  localCartToRemoteCart(){
+    let data = localStorage.getItem('localCart');
+    if(data){
+      let cartDataList: product[] = JSON.parse(data)
+      let user = localStorage.getItem('user');
+      let userId = user && JSON.parse(user).id;
+      cartDataList.forEach((product: product, index) => {
+        let cartData: cart = {
+          ...product,
+          productId: product.id,
+          userId
+        };
+        delete cartData.id;
+        setTimeout(() => {
+          this.productService.addToCart(cartData).subscribe((result) =>{
+            if(result){
+              console.log("item stored in db");
+              
+            }
+          })
+          if(cartDataList.length === index+1){
+          localStorage.removeItem('localCart')
+          }
+        }, 500)
+      })
+    }
   }
 }
